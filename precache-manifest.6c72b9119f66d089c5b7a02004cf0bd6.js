4628e99221d4f34e7deef00a029a885caa773157
Jalen-G <goodloejalen@gmail.com>
2020-08-26 22:03:58 -0500

    Updates

---
+++ precache-manifest.6c72b9119f66d089c5b7a02004cf0bd6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "295f07a894738153208875101d7edca7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69adeaf803f08932648a",
+    "url": "/static/css/main.fac152a4.chunk.css"
+  },
+  {
+    "revision": "db773adfc4f302953512",
+    "url": "/static/js/2.648bd840.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.648bd840.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69adeaf803f08932648a",
+    "url": "/static/js/main.583339c6.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "263fb93709c060d97ab415a9b7d98b85",
+    "url": "/static/media/Jalen_Goodloe_Resume.263fb937.pdf"
+  },
+  {
+    "revision": "da1fb5f1b4b420d34ade5cff66c68436",
+    "url": "/static/media/logo.da1fb5f1.svg"
+  }
+]);
